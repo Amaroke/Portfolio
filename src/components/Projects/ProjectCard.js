d6de26c731acbaed7570d53b91a5e045114a4a73
Amaroke <hugo.amaroke@gmail.com>
2023-06-06 17:30:18 +0200

    Modal for projects and css modifications

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,13 +1,19 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, resume, description, imgUrl, imgUrl1, imgUrl2, imgUrl3, imgUrl4, imgUrl5, imglang1, imglang2, imglang3, lang1, lang2, lang3, setSelectedProject, setShowDialog }) => {
+
+  const handleClick = () => {
+    setSelectedProject({ title, description, imgUrl, imgUrl1, imgUrl2, imgUrl3, imgUrl4, imgUrl5, imglang1, imglang2, imglang3, lang1, lang2, lang3 });
+    setShowDialog(true);
+  };
+
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} alt="project_image"/>
+      <div className="proj-imgbx" onClick={handleClick}>
+        <img src={imgUrl} alt="project_image" />
         <div className="proj-txtx">
           <h4>{title}</h4>
-          <span>{description}</span>
+          <span>{resume}</span>
         </div>
       </div>
     </Col>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,44 +1,83 @@
+import React, { useState } from "react";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import DOMPurify from 'dompurify';
+import { Modal, Button } from "react-bootstrap";
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
+import logoPython from "../../assets/img/logo-python.png";
+import logonumpy from "../../assets/img/logo-numpy.png";
 import projImg from "../../assets/img/project.jpg";
+import georges from "../../assets/img/georges.png";
 import ProjetDPiscine from "../../assets/img/projetDPiscine.png";
 import obstACL_tower from "../../assets/img/obstACL_tower.png";
+import goldenppit from "../../assets/img/goldenppit.png";
 import colorSharp2 from "../../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 import './Projects.css'
+import './Modal.css'
 
 export const Projects = () => {
+
+  const [showDialog, setShowDialog] = useState(false);
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  const handleCloseDialog = () => {
+    setShowDialog(false);
+  };
+
+  const settings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    focusOnSelect: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    cssEase: "linear"
+  };
 
   const projects = [
     {
       title: "Georges",
-      description: "Nettoyage d'archives",
-      imgUrl: projImg,
+      resume: "Nettoyage d'archives",
+      description: "\"Georges\" permet grâce au raisonnement à partir de cas de nettoyer des archives. Ce projet a été réalisé, dans le cadre de l'UE Initiation à la Recherche de mon Master Informatique à l'Université de Lorraine. Il a été réalisé en collaboration avec Persée, une Unité de Support et de Recherche de l'ENS de Lyon et du CNRS. Entièrement écris en Python l'outil développé à permis d'obtenir des résultats très satisfaisants sur l'ensemble des documents fournis. Ces travaux ont menés à la publication d'un article à l'occasion de l'International Conference on Case-Based Reasoning de 2023.<br>Pour plus d'informations, vous pouvez consulter le <a href=\"github.com/Amaroke/Georges\">dépôt GitHub</a> du projet.",
+      imgUrl: georges,
+      imgUrl1: georges,
+      imgUrl2: georges,
+      imgUrl3: georges,
+      imglang1: logoPython,
+      imglang2: logonumpy,
+      lang1: "Python",
+      lang2: "NumPy"
     },
     {
       title: "ProjetDPiscine",
-      description: "Site de gestion d'agenda",
+      resume: "Site de gestion d'agenda",
       imgUrl: ProjetDPiscine,
     },
     {
       title: "ObstACL-Tower",
-      description: "Jeu de roguelike",
+      resume: "Jeu de roguelike",
       imgUrl: obstACL_tower,
     },
     {
       title: "Golden-PPIT",
-      description: "Site web d'organisation d'évènements",
-      imgUrl: projImg,
+      resume: "Site web d'organisation d'évènements",
+      imgUrl: goldenppit,
     },
     {
       title: "PlatVenture",
-      description: "Jeu de plateforme",
+      resume: "Jeu de plateforme",
       imgUrl: projImg,
     },
     {
-      title: "PuissanceQuatre",  
-      description: "IA de jeu de puissance 4",
+      title: "PuissanceQuatre",
+      resume: "IA de jeu de puissance 4",
       imgUrl: projImg,
     },
   ];
@@ -46,17 +85,17 @@ export const Projects = () => {
   const perso_projects = [
     {
       title: "Charadle",
-      description: "Jeu de devinette de personnages",
+      resume: "Jeu de devinette de personnages",
       imgUrl: projImg,
     },
     {
       title: "MakeMoreMeat",
-      description: "Jeu incrémental sous Android",
+      resume: "Jeu incrémental sous Android",
       imgUrl: projImg,
     },
     {
       title: "ReactChess",
-      description: "Site d'échecs",
+      resume: "Site d'échecs",
       imgUrl: projImg,
     }
   ];
@@ -91,6 +130,8 @@ export const Projects = () => {
                                 <ProjectCard
                                   key={index}
                                   {...project}
+                                  setSelectedProject={setSelectedProject}
+                                  setShowDialog={setShowDialog}
                                 />
                               )
                             })
@@ -98,16 +139,18 @@ export const Projects = () => {
                         </Row>
                       </Tab.Pane>
                       <Tab.Pane eventKey="second">
-                      <p>Voici les différents projets dans lesquels je suis investis sur mon temps libre :</p>
+                        <p>Voici les différents projets dans lesquels je suis investis sur mon temps libre :</p>
                       </Tab.Pane>
                       <Tab.Pane eventKey="second">
-                      <Row>
+                        <Row>
                           {
                             perso_projects.map((project, index) => {
                               return (
                                 <ProjectCard
                                   key={index}
                                   {...project}
+                                  setSelectedProject={setSelectedProject}
+                                  setShowDialog={setShowDialog}
                                 />
                               )
                             })
@@ -120,8 +163,58 @@ export const Projects = () => {
             </TrackVisibility>
           </Col>
         </Row>
+        <Modal show={showDialog} onHide={handleCloseDialog}>
+          <Modal.Header closeButton className="custom-close-button">
+            <Modal.Title>{selectedProject?.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Row>
+              <Col size={14} sm={7}>
+                <div className="image-container">
+                  <Slider {...settings} style={{ width: '50vw', dotStyle: { backgroundColor: 'white' }, activeDotStyle: { backgroundColor: 'white' } }}>
+                    <img src={selectedProject?.imgUrl1} alt={selectedProject?.title} />
+                    {selectedProject?.imgUrl2 && <img src={selectedProject?.imgUrl2} alt={selectedProject?.title} />}
+                    {selectedProject?.imgUrl3 && <img src={selectedProject?.imgUrl3} alt={selectedProject?.title} />}
+                    {selectedProject?.imgUrl4 && <img src={selectedProject?.imgUrl4} alt={selectedProject?.title} />}
+                    {selectedProject?.imgUrl5 && <img src={selectedProject?.imgUrl5} alt={selectedProject?.title} />}
+                  </Slider>
+                </div>
+              </Col>
+              <Col size={10} sm={5}>
+                <div className="text-container">
+                  <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(selectedProject?.description) }}></div>
+                </div>
+                <div className="technos">
+                  Technologies utilisées :
+                </div>
+                <div className="icons-container">
+                  <div className="languages">
+                    <Row>
+                      <Col>
+                        <img src={selectedProject?.imglang1} alt={selectedProject?.lang1} />
+                        <p>{selectedProject?.lang1}</p>
+                      </Col>
+                      <Col>
+                        {selectedProject?.lang2 && <img src={selectedProject?.imglang2} alt={selectedProject?.lang2} />}
+                        {selectedProject?.lang2 && <p>{selectedProject?.lang2}</p>}
+                      </Col>
+                      <Col>
+                        {selectedProject?.lang3 && <img src={selectedProject?.imglang2} alt={selectedProject?.lang3} />}
+                        {selectedProject?.lang2 && <p>{selectedProject?.lang3}</p>}
+                      </Col>
+                    </Row>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseDialog}>Fermer</Button>
+          </Modal.Footer>
+        </Modal>
+
       </Container>
       <img className="background-image-right" src={colorSharp2} alt="background"></img>
-    </section>
+    </section >
   )
 }
