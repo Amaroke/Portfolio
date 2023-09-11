797112d9f2b6ab978889485aeb212b1b2dc3e24e
Amaroke <hugo.amaroke@gmail.com>
2023-09-11 21:34:51 +0200

    Separation Modal and Project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,7 @@
 import React, { useState } from "react";
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import DOMPurify from 'dompurify';
-import { Modal, Button } from "react-bootstrap";
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./components/project_card/ProjectCard";
+import ModalComponent from "./components/modal/ModalComponent";
 import 'animate.css';
 import './projects.css'
 import './components/modal/modal.css'
@@ -14,37 +10,6 @@ import { projects, perso_projects } from './datas/ProjectsDatas';
 function Projects() {
   const [showDialog, setShowDialog] = useState(false);
   const [selectedProject, setSelectedProject] = useState(null);
-
-  const handleCloseDialog = () => {
-    setShowDialog(false);
-  };
-
-  const settings = {
-    dots: true,
-    arrows: false,
-    infinite: true,
-    autoplay: true,
-    focusOnSelect: false,
-    pauseOnFocus: false,
-    pauseOnHover: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    cssEase: "linear"
-  };
-
-  const {
-    imgUrl1,
-    imgUrl2,
-    imgUrl3,
-    imgUrl4,
-    imgUrl5,
-    imglang1,
-    imglang2,
-    imglang3,
-    lang1,
-    lang2,
-    lang3,
-  } = selectedProject || {};
 
   return (
     <section className="project" id="project">
@@ -120,74 +85,12 @@ function Projects() {
             </Tab.Container>
           </Col>
         </Row>
-        <Modal show={showDialog} onHide={handleCloseDialog}>
-          <Modal.Header closeButton className="custom-close-button">
-            <Modal.Title>{selectedProject?.title}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Row>
-              <Col size={14} sm={7}>
-                <div className="image-container">
-                  <Slider
-                    {...settings}
-                    style={{
-                      width: '50vw',
-                      dotStyle: { backgroundColor: 'white' },
-                      activeDotStyle: { backgroundColor: 'white' },
-                    }}
-                  >
-                    <img src={imgUrl1} alt={selectedProject?.title} />
-                    {imgUrl2 && <img src={imgUrl2} alt={selectedProject?.title} />}
-                    {imgUrl3 && <img src={imgUrl3} alt={selectedProject?.title} />}
-                    {imgUrl4 && <img src={imgUrl4} alt={selectedProject?.title} />}
-                    {imgUrl5 && <img src={imgUrl5} alt={selectedProject?.title} />}
-                  </Slider>
-                </div>
-              </Col>
-              <Col size={10} sm={5}>
-                <div className="text-container">
-                  <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(selectedProject?.description) }}></div>
-                </div>
-                <div className="technos">
-                  {"en" === "en"
-                    ? "Technologies used:"
-                    : "Technologies utilisées :"}
-                </div>
-                <div className="icons-container">
-                  <div className="languages">
-                    <Row>
-                      <Col>
-                        <div class="language-container">
-                          <img src={imglang1} alt={lang1} />
-                          <p>{lang1}</p>
-                        </div>
-                      </Col>
-                      <Col>
-                        <div class="language-container">
-                          {imglang2 && <img src={imglang2} alt={lang2} />}
-                          {imglang2 && <p>{lang2}</p>}
-                        </div>
-                      </Col>
-                      <Col>
-                        <div class="language-container">
-                          {imglang3 && <img src={imglang3} alt={lang3} />}
-                          {imglang3 && <p>{lang3}</p>}
-                        </div>
-                      </Col>
-                    </Row>
-                  </div>
-                </div>
-              </Col>
-            </Row>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleCloseDialog}>
-              {"en" === "en"
-                ? "Close"
-                : "Fermer"}
-            </Button>
-          </Modal.Footer>
-        </Modal>
+        <ModalComponent
+          showDialog={showDialog}
+          selectedProject={selectedProject}
+          setShowDialog={setShowDialog}
+          setSelectedProject={setSelectedProject}
+        />
       </Container>
     </section >
   );
---
+++ src/components/Projects/components/modal/ModalComponent.js
@@ -0,0 +1,113 @@
+import React from "react";
+import { Modal, Button, Row, Col } from "react-bootstrap";
+import Slider from 'react-slick';
+import DOMPurify from 'dompurify';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+function ModalComponent(props) {
+    const closeModal = () => {
+        props.setShowDialog(false);
+        props.setSelectedProject(null);
+    };
+
+    const settings = {
+        dots: true,
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        focusOnSelect: false,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        cssEase: "linear"
+    };
+
+    const {
+        imgUrl1,
+        imgUrl2,
+        imgUrl3,
+        imgUrl4,
+        imgUrl5,
+        imglang1,
+        imglang2,
+        imglang3,
+        lang1,
+        lang2,
+        lang3,
+    } = props.selectedProject || {};
+
+    return (
+        <Modal show={props.showDialog} onHide={closeModal}>
+            <Modal.Header closeButton className="custom-close-button">
+                <Modal.Title>{props.selectedProject?.title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Row>
+                    <Col size={14} sm={7}>
+                        <div className="image-container">
+                            <Slider
+                                {...settings}
+                                style={{
+                                    width: '50vw',
+                                    dotStyle: { backgroundColor: 'white' },
+                                    activeDotStyle: { backgroundColor: 'white' },
+                                }}
+                            >
+                                <img src={imgUrl1} alt={props.selectedProject?.title} />
+                                {imgUrl2 && <img src={imgUrl2} alt={props.selectedProject?.title} />}
+                                {imgUrl3 && <img src={imgUrl3} alt={props.selectedProject?.title} />}
+                                {imgUrl4 && <img src={imgUrl4} alt={props.selectedProject?.title} />}
+                                {imgUrl5 && <img src={imgUrl5} alt={props.selectedProject?.title} />}
+                            </Slider>
+                        </div>
+                    </Col>
+                    <Col size={10} sm={5}>
+                        <div className="text-container">
+                            <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(props.selectedProject?.description) }}></div>
+                        </div>
+                        <div className="technos">
+                            {"en" === "en"
+                                ? "Technologies used:"
+                                : "Technologies utilisées :"}
+                        </div>
+                        <div className="icons-container">
+                            <div className="languages">
+                                <Row>
+                                    <Col>
+                                        <div class="language-container">
+                                            <img src={imglang1} alt={lang1} />
+                                            <p>{lang1}</p>
+                                        </div>
+                                    </Col>
+                                    <Col>
+                                        <div class="language-container">
+                                            {imglang2 && <img src={imglang2} alt={lang2} />}
+                                            {imglang2 && <p>{lang2}</p>}
+                                        </div>
+                                    </Col>
+                                    <Col>
+                                        <div class="language-container">
+                                            {imglang3 && <img src={imglang3} alt={lang3} />}
+                                            {imglang3 && <p>{lang3}</p>}
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={closeModal}>
+                    {"en" === "en"
+                        ? "Close"
+                        : "Fermer"}
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+export default ModalComponent;
